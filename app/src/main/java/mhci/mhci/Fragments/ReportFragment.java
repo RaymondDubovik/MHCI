01e91cd06cd912a4ba8f7c218b19c762012b4d1c
tomazz <tomsz.sadowski@gmail.com>
2016-03-10 21:51:32 +0000

    added voting and reporting views

--- app/src/main/java/mhci/mhci/Fragments/ReportFragment.java
+++ app/src/main/java/mhci/mhci/Fragments/ReportFragment.java
@@ -1,16 +1,70 @@
 package mhci.mhci.Fragments;
 
 import android.app.Fragment;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
+import mhci.mhci.MainActivity;
 import mhci.mhci.R;
 
-public class ReportFragment extends Fragment {
-    public ReportFragment() {
-        // Required empty public constructor
+public class ReportFragment extends Fragment implements MenuListener {
+
+    private RadioGroup radioGroup;
+    private EditText mEdit;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_report, container, false);
+
+        MainActivity activity = (MainActivity) getActivity();
+        activity.registerReceiver(new MenuReceiver(this), new IntentFilter(MainActivity.INTENT_FILTER_FRAGMENT_MENU_CLICK));
+        if (activity.isMenuPressed()) {
+            onMenuPressed();
+        }
+        Button submitButton = (Button) view.findViewById(R.id.reportBtn);
+
+        radioGroup = (RadioGroup) view.findViewById(R.id.radioGroup);
+        mEdit = (EditText)view.findViewById(R.id.editTextReport);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // get selected radio button from radioGroup
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+                String editTextContent = mEdit.getText().toString();
+                Toast.makeText(getActivity(),"Thanks for reporting, good citizen!",Toast.LENGTH_LONG).show();
+                radioGroup.clearCheck();
+                mEdit.setText(null);
+                InputMethodManager imm = (InputMethodManager)
+                ReportFragment.this.getActivity().getSystemService(ReportFragment.this.getActivity().INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+            }
+        });
+        return view;
+    }
+
+
+    @Override
+    public void onMenuPressed() {
+
+    }
+
+
+    @Override
+    public void onMenuDepressed() {
+
     }
 
 
@@ -20,9 +74,4 @@ public class ReportFragment extends Fragment {
     }
 
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_report, container, false);
-    }
 }
--- app/src/main/java/mhci/mhci/Fragments/VoteFragment.java
+++ app/src/main/java/mhci/mhci/Fragments/VoteFragment.java
@@ -5,14 +5,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import mhci.mhci.R;
 
 public class VoteFragment extends Fragment {
-    public VoteFragment() {
-        // Required empty public constructor
-    }
 
+
+    private RadioGroup radioGroup1;
+    private RadioGroup radioGroup2;
+    private RadioGroup radioGroup3;
+    private Button submitButton;
+    private ArrayList<RadioGroup> questions;
+    private View view;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,7 +33,40 @@ public class VoteFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_vote, container, false);
+        submitButton = (Button) view.findViewById(R.id.voteBtn);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hide();
+                Toast.makeText(getActivity(), "Thanks for your vote, good citizen!", Toast.LENGTH_LONG).show();
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_vote, container, false);
+        return view;
     }
+
+    private void hide() {
+
+        if (((RadioButton) view.findViewById(R.id.rb1)).isChecked()
+                || ((RadioButton) view.findViewById(R.id.rb2)).isChecked()
+                ) {
+            view.findViewById(R.id.ll1).setVisibility(View.GONE);
+        }
+
+        if (((RadioButton) view.findViewById(R.id.rb3)).isChecked()
+                || ((RadioButton) view.findViewById(R.id.rb4)).isChecked()
+                ) {
+            view.findViewById(R.id.ll2).setVisibility(View.GONE);
+        }
+
+        if (((RadioButton) view.findViewById(R.id.rb5)).isChecked()
+                || ((RadioButton) view.findViewById(R.id.rb6)).isChecked()
+                ) {
+            view.findViewById(R.id.ll3).setVisibility(View.GONE);
+        }
+    }
+
 }
